2467128556dc5498c852331b3b26289f16a2ad3a
Christian Coffey <coffeychristian@gmail.com>
2021-05-21 16:02:47 -0400

    Create/Get votes by game session id
    - Added a bunch of boilerplate for cosmos db + EF core + Swagger
    - Functionality for creating vote documents in the db
    - Functionality for looking up those documents by the game session id

---
+++ AppServiceDemo/Controllers/VoteController.cs
@@ -0,0 +1,60 @@
+﻿using AppServiceDemo.Data.Models;
+using AppServiceDemo.Data.Repository;
+using AppServiceDemo.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppServiceDemo.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class VoteController : ControllerBase
+	{
+		private readonly ILogger<VoteController> _logger;
+		private readonly IVoteService _voteService;
+
+		public VoteController(
+			ILogger<VoteController> logger,
+			IVoteService voteService)
+		{
+			_logger = logger;
+			_voteService = voteService;
+		}
+
+		[HttpGet]
+		[ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Vote>))]
+		[Route("gamesession/{gameSessionId:guid}")]
+		public async Task<ActionResult<IEnumerable<Vote>>> Get(Guid gameSessionId)
+		{
+			try
+			{
+				return Ok(await _voteService.GetByGameSessionIdAsync(gameSessionId));
+			}
+			catch (Exception e)
+			{
+				_logger.LogError($"An error occurred fetching votes for game session {gameSessionId}", e);
+				return StatusCode(500);
+			}
+		}
+
+		[HttpPost]
+		public async Task<ActionResult<Vote>> Create(Guid gameSessionId, int points)
+		{
+			try
+			{
+				var vote = await _voteService.CreateAsync(gameSessionId, points);
+				return CreatedAtAction(nameof(Create), new { id = vote.Id }, vote);
+			}
+			catch (Exception e)
+			{
+				_logger.LogError($"An error occurred creating vote for game session {gameSessionId}", e);
+				return StatusCode(500);
+			}
+		}
+	}
+}
---
+++ AppServiceDemo/Data/Repository/VoteRepository.cs
@@ -0,0 +1,47 @@
+﻿using AppServiceDemo.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppServiceDemo.Data.Repository
+{
+	public interface IVoteRepository
+	{
+		Task<Vote> CreateAsync(Guid gameSessionId, int points);
+		Task<IEnumerable<Vote>> GetByGameSessionIdAsync(Guid gameSessionId);
+	}
+
+	public class VoteRepository : IVoteRepository
+	{
+		private readonly CosmosDbContext _dbContext;
+
+		public VoteRepository(CosmosDbContext dbContext)
+		{
+			_dbContext = dbContext;
+			_dbContext.Database.EnsureCreated();
+		}
+
+		public async Task<Vote> CreateAsync(Guid gameSessionId, int points)
+		{
+			var entity = _dbContext.Votes.Add(new Vote()
+			{
+				Points = points,
+				GameSessionId = gameSessionId,
+				CreatedAt = DateTime.Now
+			}).Entity;
+
+			await _dbContext.SaveChangesAsync();
+
+			return entity;
+		}
+
+		public async Task<IEnumerable<Vote>> GetByGameSessionIdAsync(Guid gameSessionId)
+		{
+			return await _dbContext.Votes
+				.Where(x => x.GameSessionId == gameSessionId)
+				.ToArrayAsync();
+		}
+	}
+}
