a1ae437f426c96138b5f01bedfa84f1c48d34df3
Christian Coffey <coffeychristian@gmail.com>
2021-07-09 17:22:53 -0400

    Completely regenerated the angular app to bring it up to date; added simple UI and a modal.

--- AppServiceDemo/ClientApp/src/app/app.component.ts
+++ AppServiceDemo/ClientApp/src/app/app.component.ts
@@ -1,9 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  encapsulation: ViewEncapsulation.None,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
+
+  openBackDropCustomClass(content) {
+    this.modalService.open(content, { backdropClass: 'blur-backdrop' });
+  }
 }
