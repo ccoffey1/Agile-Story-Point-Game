60d5b5915657231bdb6b341090d5545e4cd218d5
Christian Coffey <coffeychristian@gmail.com>
2021-07-10 15:35:37 -0400

    Ditched animations for now... Keep it simple. :angry:

--- AppServiceDemo/ClientApp/src/app/app.component.ts
+++ AppServiceDemo/ClientApp/src/app/app.component.ts
@@ -1,18 +1,36 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  encapsulation: ViewEncapsulation.None,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  closeResult: string;
+
+  showMainMenu = true
+  showJoinTeam = false
+  showCreateTeam = false
 
   constructor(private modalService: NgbModal) {}
 
   openBackDropCustomClass(content) {
     this.modalService.open(content, { backdropClass: 'blur-backdrop' });
+  }
+
+  joinTeamClicked() {
+    this.showMainMenu = false;
+    this.showJoinTeam = true;
+  }
+
+  createTeamClicked() {
+    this.showMainMenu = false;
+    this.showCreateTeam = true;
+  }
+
+  backClicked() {
+    this.showCreateTeam = false;
+    this.showJoinTeam = false;
+    this.showMainMenu = true;
   }
 }
