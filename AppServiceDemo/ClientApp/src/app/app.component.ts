a72921ecbc28c7fe2d027de08d48eee868644f58
Christian Coffey <31052077+ccoffey1@users.noreply.github.com>
2021-07-10 15:40:10 -0400

    Feature/#15 UI join create team (#16)
    * Completely regenerated the angular app to bring it up to date; added simple UI and a modal.
    
    * Ditched animations for now... Keep it simple. :angry:

--- AppServiceDemo/ClientApp/src/app/app.component.ts
+++ AppServiceDemo/ClientApp/src/app/app.component.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  showMainMenu = true
+  showJoinTeam = false
+  showCreateTeam = false
+
+  constructor(private modalService: NgbModal) {}
+
+  openBackDropCustomClass(content) {
+    this.modalService.open(content, { backdropClass: 'blur-backdrop' });
+  }
+
+  joinTeamClicked() {
+    this.showMainMenu = false;
+    this.showJoinTeam = true;
+  }
+
+  createTeamClicked() {
+    this.showMainMenu = false;
+    this.showCreateTeam = true;
+  }
+
+  backClicked() {
+    this.showCreateTeam = false;
+    this.showJoinTeam = false;
+    this.showMainMenu = true;
+  }
 }
