fa617d6df834fe25d11b89c6379f79b8a96e4350
Christian Coffey <coffeychristian@gmail.com>
2021-07-16 16:53:02 -0400

    Reasons

--- AppServiceDemo/ClientApp/src/app/game-stage/game-stage.component.ts
+++ AppServiceDemo/ClientApp/src/app/game-stage/game-stage.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameStageComponent implements OnInit {
 
+  reasons: any[] = [
+    { title: '¯\\_(ツ)_/¯', description: "I have no idea."},
+    { title: 'New Territory', description: "We're unfamiliar with this area."},
+    { title: 'Minimal Dev Work', description: "Won't take too long."},
+    { title: 'Medium Dev Work', description: "Quite a bit of work."},
+    { title: 'Tons of Dev Work', description: "Hopefully we'll finish by retirement."},
+    { title: 'QA Buffer', description: "More time for QE work."},
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
